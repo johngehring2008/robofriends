ea911859916e198f33a12105ab43e172f71d15cd
John Gehring <john.gehring@walgreens.com>
2020-12-09 00:19:22 -0600

    Added external font and background with linear gradient

---
+++ src/robots.js
@@ -0,0 +1,64 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'Steven Nelson',
+    username: 'slnelshm',
+    biography: '',
+    phone: '',
+    email: 'steven.nelson@walgreens.com'
+  },
+  {
+    id: 2,
+    name: 'Keith Sturm',
+    username: 'kasturm',
+    email: 'keith.sturm@walgreens.com'
+  },
+  {
+    id: 3,
+    name: 'Rick Dresen',
+    username: 'rwdres5j',
+    email: 'rick.dresen@walgreens.com'
+  },
+  {
+    id: 4,
+    name: 'Gina Graville',
+    username: 'ggravill',
+    email: 'gina.graville@walgreens.com'
+  },
+  {
+    id: 5,
+    name: 'John Gehring',
+    username: 'jgehring',
+    email: 'john.gehring@walgreens.com'
+  },
+  {
+    id: 6,
+    name: 'Lloyd Keen',
+    username: 'lkeen',
+    email: 'lloyd.keen@walgreens.com'
+  },
+  {
+    id: 7,
+    name: 'Jon Maly',
+    username: 'jjmaly',
+    email: 'jon.maly@walgreens.com'
+  },
+  {
+    id: 8,
+    name: 'Ryne Collies',
+    username: 'rrcollaj',
+    email: 'ryne.collies@walgreens.com'
+  },
+  {
+    id: 9,
+    name: 'Cheryl Ogle',
+    username: 'coglehm',
+    email: 'cheryl.ogle@walgreens.com'
+  },
+  {
+    id: 10,
+    name: 'Rick Walton',
+    username: 'rawaltzr',
+    email: 'rick.walton@walgreens.com'
+  }
+];
